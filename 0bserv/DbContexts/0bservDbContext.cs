acf26eecda886880ad783663a97f24193e8c5cd9
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-30 19:18:27 +0100

    configurazioni varie

---
+++ 0bserv/DbContexts/0bservDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using _0bserv.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace _0bserv.DbContexts
+{
+    public class _0bservDbContext : DbContext
+    {
+        public DbSet<RssFeed> RssFeeds { get; set; }
+        public DbSet<FeedContent> FeedContents { get; set; }
+
+        public _0bservDbContext(DbContextOptions<_0bservDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information); // Abilita il logging delle query su Console
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            _ = modelBuilder.Entity<RssFeed>()
+                .HasKey(r => r.Id);
+            _ = modelBuilder.Entity<FeedContent>()
+                .HasKey(f => f.Id);
+            _ = modelBuilder.Entity<FeedContent>()
+                .HasOne(f => f.RssFeed)
+                .WithMany(r => r.Contents)
+                .HasForeignKey(f => f.RssFeedId);
+            _ = modelBuilder.Entity<RssFeed>()
+                .Property(r => r.Url)
+                .IsRequired()
+                .HasMaxLength(255);
+            _ = modelBuilder.Entity<FeedContent>()
+                .HasIndex(f => f.PublishDate);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
+
---
+++ 0bserv/Models/FeedModel.cs
@@ -0,0 +1,51 @@
+﻿namespace _0bserv.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class RssFeed
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Url { get; set; }
+
+        // Altre proprietà se necessario
+
+        // Relazione con il contenuto dei feed
+        public virtual ICollection<FeedContent> Contents { get; set; }
+    }
+
+    public class FeedContent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int RssFeedId { get; set; }
+
+        [ForeignKey("RssFeedId")]
+        public virtual RssFeed RssFeed { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Link { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public DateTime PublishDate { get; set; }
+
+        // Altre proprietà se necessario
+    }
+}
