1abf9fc1c7f33faec0fb622d9a279d8f8cced95d
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-30 16:49:41 +0100

    db context

---
+++ 0bserv/DbContext/0bservDb.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Observ.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RssFeed> RssFeeds { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+
+    public class RssFeed
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public int RssFeedId { get; set; }
+        public RssFeed RssFeed { get; set; }
+    }
+}
